ce1f601f33362432639869292149fd5c97135526
Tomokiyo-H <2301179@s.asojuku.ac.jp>
2024-12-10 10:08:01 +0900

    todoリストの完成

--- Todo/script/script.js
+++ Todo/script/script.js
@@ -2,15 +2,30 @@ new Vue({
   el: '#app',
   data() {
     return {
-      //データの準備
+      todos:[],
+      text:''
 
     };
   },
   methods: {
+    inputText(e){
+      this.text=e.target.value;
+    },
     //inputTextメソッドを追加
-
+    addTodo(){
+      if(!this.text)return;
+      const text =this.text;
+      const id =Math.ceil(Math.random()*1000);
+      const todo ={
+        id, text, isDone : false
+      };
+      this.todos.push(todo);
+      this.resetText();
+    },
     //addTodoメソッドを追加
-
+    resetText(){
+      this.text ='';
+    }
     //resetTextメソッドを追加
     
   }
