529362dbf06e8cf699c05cdf6f12b99d4435147e
Tomokiyo-H <2301179@s.asojuku.ac.jp>
2024-12-17 10:05:38 +0900

    Todoの完成

--- Todo/script/script.js
+++ Todo/script/script.js
@@ -2,51 +2,51 @@ new Vue({
   el: '#app',
   data() {
     return {
-      todos:[],
-      text:''
-
+      //データの準備
+      todos: [],
+      text: ''
     };
   },
   methods: {
-    inputText(e){
+    //inputTextメソッドを追加
+    inputText(e) {
       this.text=e.target.value;
     },
-    //inputTextメソッドを追加
+    //addTodoメソッドを追加
     addTodo(){
       if(!this.text)return;
-      const text =this.text;
-      const id =Math.ceil(Math.random()*1000);
-      const todo ={
-        id, text, isDone : false
+      const text = this.text;
+      const id = Math.ceil(Math.random() * 1000);
+      const todo = {
+        id,text,isDone: false
       };
       this.todos.push(todo);
       this.resetText();
     },
-    //addTodoメソッドを追加
-    resetText(){
-      this.text ='';
-    }
     //resetTextメソッドを追加
-  },
-  deleteTodo(id){
-    const index =this.getIndexBy(id);
-    this.todos.splice(index,1);
-  },
-  toggleIsDone(id){
-    const index =this.getIndexBy(id);
-    this.todos[index].isDone = !this.todo[index].isDone;
-  },
-  getIndexBy(id){
-    const filteredTodo =this.todos.filter(todo =>todo.id === id)[0];
+    resetText(){
+      this.text='';
+    },
+    deleteTodo(id){
+      const index = this.getIndexBy(id);
+      this.todos.splice(index, 1);
+    },
+    toggleIsDone(id){
+      const index = this.getIndexBy(id);
+      this.todos[index].isDone=!this.todos[index].isDone;
+    },
+    getIndexBy(id){
+    const filteredTodo = this.todos.filter(todo => todo.id === id)[0];
     const index = this.todos.indexOf(filteredTodo);
     return index;
-  },
-  computed:{
-    doneTodo(){
-      return this.todos.filter(todo =>isDone === true);
-    },
-    incompleteTodo(){
-      return this.todos.filter(todo => todo.isDone === false);
     }
+  },
+computed: {
+  doneTodo(){
+    return this.todos.filter( todo=>todo.isDone === true);
+  },
+  incompleteTodo(){
+    return this.todos.filter( todo=>todo.isDone === false);
+  }
   }
 });
